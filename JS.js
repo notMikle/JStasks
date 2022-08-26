c0d5dda59cef5c5925a7a85e6b7cda116b918853
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2022-08-26 14:34:00 +0300

    Add task makeNegative

--- JS.js
+++ JS.js
@@ -7,3 +7,26 @@ function evenOrOdd(number) {
 
 // console.log(evenOrOdd(2))
 // console.log(evenOrOdd(3))
+
+
+
+// ---2---
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+//
+//     Examples
+//     makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function makeNegative(num) {
+    return num < 0 ? num : -num;
+}
+
+// console.log(makeNegative(3))
+// console.log(makeNegative(-3))
+
