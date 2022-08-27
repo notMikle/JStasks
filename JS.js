924d24785d1d5e3ea45f5780f54c7fb7a2ecccf2
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2022-08-27 15:41:00 +0300

    Add task sum from array

--- JS.js
+++ JS.js
@@ -30,3 +30,25 @@ function makeNegative(num) {
 // console.log(makeNegative(3))
 // console.log(makeNegative(-3))
 
+
+// ---3---
+
+
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+//
+// Examples
+// Input: [1, 5.2, 4, 0, -1]
+// Output: 9.2
+//
+// Input: []
+// Output: 0
+//
+// Input: [-2.398]
+// Output: -2.398
+
+
+function sum(numbers) {
+    return numbers.reduce((a, b) => a + b, 0);
+}
+
+// console.log(sum([1,2,3,4]))
