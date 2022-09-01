3045f0fa0095674730566a00f3d536068fbfe87d
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2022-09-01 16:21:00 +0300

    Add task reverseSeq

--- JS.js
+++ JS.js
@@ -69,4 +69,18 @@ function sum(numbers) {
 
 const summation = n => n * (n + 1) / 2;
 
+// ---5---
 
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+//
+// Example : n=5 --> [5,4,3,2,1]
+//
+
+
+const reverseSeq = n => {
+    let arr = [];
+    for (let i=n; i>0; i--) {
+        arr.push(i);
+    } return arr;
+};
