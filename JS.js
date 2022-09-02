1b2206488276267c45342c975b22d6be6a442c5b
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2022-09-02 12:11:00 +0300

    Add task solution reverse string

--- JS.js
+++ JS.js
@@ -84,3 +84,21 @@ const reverseSeq = n => {
         arr.push(i);
     } return arr;
 };
+
+
+
+
+
+// ---6---
+
+
+
+// Complete the solution so that it reverses the string passed into it.
+//
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+function solution(str){
+    return str.split('').reverse().join('');
+}
