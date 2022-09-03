b7a979557cd97e5224a8af72e7c2a7a7c86cbb38
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2022-09-03 11:34:00 +0300

    Add task SmallestIntegerFinder

--- JS.js
+++ JS.js
@@ -102,3 +102,22 @@ const reverseSeq = n => {
 function solution(str){
     return str.split('').reverse().join('');
 }
+
+
+
+// ---7---
+//
+// Given an array of integers your solution should find the smallest integer.
+//
+//     For example:
+//
+//     Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+//
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return Math.min(...args)
+    }
+}
