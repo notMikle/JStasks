6af475ae13ef06fcece81347a06487401bd498b4
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2022-09-05 17:32:00 +0300

    Add task findNeedle

--- JS.js
+++ JS.js
@@ -137,3 +137,23 @@ class SmallestIntegerFinder {
 // Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
 
 const areaOrPerimeter = (l , w) => l === w ? l*w : 2*(l+w);
+
+
+// ---9---
+
+// Can you find the needle in the haystack?
+//
+//     Write a function findNeedle() that takes an array full of junk but containing one "needle"
+//
+// After your function finds the needle it should return a message (as a string) that says:
+//
+//     "found the needle at position " plus the index it found the needle, so:
+//
+// Example(Input --> Output)
+//
+//     ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5"
+// Note: In COBOL, it should return "found the needle at position 6"
+
+function findNeedle(haystack) {
+    return "found the needle at position " + haystack.indexOf("needle");
+}
