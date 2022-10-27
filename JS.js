f107d05ecbfe99e98ba4d152de79127d767101ac
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2022-10-27 18:31:00 +0300

    Add task calculator

--- JS.js
+++ JS.js
@@ -241,3 +241,31 @@ function abbrevName(name){
 // The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 
 const DNAtoRNA = dna => dna.replace(/T/g, 'U');
+
+
+
+// ---16---
+// Your task is to create a function that does four basic mathematical operations.
+//
+//     The function should take three arguments - operation(string/char), value1(number), value2(number).
+//     The function should return result of numbers after applying the chosen operation.
+//
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+        default:
+            return 0;
+    }
+}
