aa1cccdce00f2b744d07a30fcd1ce102c5ddfe8f
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2022-12-03 10:47:00 +0300

    Add task addBinary

--- JS.js
+++ JS.js
@@ -302,3 +302,16 @@ function litres(time) {
 function find_difference(a, b) {
     return Math.abs(a[0]*a[1]*a[2]-b[0]*b[1]*b[2]);
 }
+
+// ---19---
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+//
+//     The binary number returned should be a string.
+//
+//     Examples:(Input1, Input2 --> Output (explanation)))
+//
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+function addBinary(a,b){
+    return (a+b).toString(2)
+}
