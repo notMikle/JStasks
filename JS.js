34a13457329d4915bc03451a318aba19a604edcd
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2023-03-30 22:18:00 +0300

    Add task getMiddle

--- JS.js
+++ JS.js
@@ -400,6 +400,29 @@ function highAndLow(numbers){
 //
 // Input: 123456789 Output: 987654321
 
+
+
 function descendingOrder(n){
     return parseInt(String(n).split('').sort().reverse().join(''))
+}
+
+
+
+// ---26---
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+//
+//     #Examples:
+//
+// Kata.getMiddle("test") should return "es"
+//
+// Kata.getMiddle("testing") should return "t"
+//
+// Kata.getMiddle("middle") should return "dd"
+//
+// Kata.getMiddle("A") should return "A"
+
+
+function getMiddle(s)
+{
+    return s.slice((s.length-1)/2, s.length/2+1);
 }
