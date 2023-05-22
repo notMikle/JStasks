ccb2e2f934dbe1ae6f3d3cc4ff692fdaf0e5d686
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2023-05-22 18:41:00 +0300

    Add task Shortest Word

--- JS.js
+++ JS.js
@@ -516,7 +516,7 @@ function XO(str) {
 }
 
 
-
+// ---31---
 // Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
 //
 // Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
@@ -529,3 +529,13 @@ function XO(str) {
 String.prototype.toJadenCase = function() {
     return this.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ')
 };
+
+
+
+// ---32---
+// Simple, given a string of words, return the length of the shortest word(s).
+//
+//     String will never be empty and you do not need to account for different data types.
+function findShort(s){
+    return Math.min(...s.split(" ").map (s => s.length));
+}
