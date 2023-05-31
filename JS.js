d5bf1bf2712b1967c34c2dfcf155df7dc24ccdff
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2023-05-31 19:23:00 +0300

    Add task Complementary DNA

--- JS.js
+++ JS.js
@@ -539,3 +539,21 @@ String.prototype.toJadenCase = function() {
 function findShort(s){
     return Math.min(...s.split(" ").map (s => s.length));
 }
+
+
+
+// ---33---
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+//
+//     If you want to know more: http://en.wikipedia.org/wiki/DNA
+//
+//     In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+//
+//     More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+//
+//     Example: (input --> output)
+//
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+let pairs = {A:'T',T:'A',C:'G',G:'C'};
+const DNAStrand = dna => dna.replace(/./g, c => pairs[c]);
