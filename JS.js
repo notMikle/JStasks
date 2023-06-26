912ecb4ea1326c28dab1ff5257422da6565925d1
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2023-06-26 09:12:00 +0300

    Add task Two to One

--- JS.js
+++ JS.js
@@ -612,5 +612,21 @@ function GetSum(a,b)
     return (Math.abs(a - b) + 1) * (a+b) / 2;
 }
 
+// ---37---
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+//
+//     Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+//
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+    return Array.from(new Set(s1 + s2)).sort().join('');
+}
+
+
 
 
