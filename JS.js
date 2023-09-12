a82be38ee299596b09549d07f886f614b5620d07
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2023-09-12 09:31:00 +0300

    Add task Testing 1-2-3

--- JS.js
+++ JS.js
@@ -729,6 +729,23 @@ function reverseWords(str) {
 
 
 
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+//
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+//
+//     The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+//
+//     Examples: (Input --> Output)
+//
+//     [] --> []
+//     ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+let number1 = function(array) {
+    return array.map(function (line, index) {
+        return (index + 1) + ": " + line;
+    });
+}
+
+
 
 
 
